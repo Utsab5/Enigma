bc2a7fa7f8ac275f7a19ee3003de0d1c8a149aae
Utsab Kundu <kunutsab@gmail.com>
2023-07-31 02:10:10 +0530

    making navbar 2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,30 @@
-import React from "react";
-//App.css
+import React, { useState } from "react";
+import {FiMenu} from 'react-icons/fi';
+import {MdClose} from 'react-icons/md';
+
 function Navbar() {
+    const [Toggle, showMenu ] = useState(false);
     return (
-        <div>this is Navbar</div>
+        <div className="nav">
+            <div className="nav_container">    
+                <div className="nav_name">
+                    <img src="../images/enigma.png" alt="logo" />
+                    <p>Enigma</p>
+                </div>
+                <div className={Toggle ? "nav_menu show-menu": "nav_menu"}>
+                    <ul>
+                        <li><a href="#portfolio">Portfolio</a></li>
+                        <li><a href="#aboutus">About Us</a></li>
+                        <li><a href="#contactus">Contact Us</a></li>
+                    </ul>
+                </div>
+                {Toggle ? 
+                <MdClose className="close" onClick={()=> showMenu(!Toggle)}/> : 
+                <FiMenu className="menu" onClick={()=> showMenu(!Toggle)}/>
+                }
+            </div>
+        </div>
+        
     );
 }
 export default Navbar;

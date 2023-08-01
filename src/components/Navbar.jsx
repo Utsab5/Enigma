2865f413d977b7ffb5a4a3a933cdf190bc91ef51
Utsab Kundu <kunutsab@gmail.com>
2023-08-01 19:58:32 +0530

    navbar complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from "react";
-import {Link} from "react-scroll"
+import React, { useState } from "react";
 import {FiMenu} from 'react-icons/fi';
 import {MdClose} from 'react-icons/md';
 //App.css
@@ -23,7 +22,10 @@ function Navbar() {
         <nav className={`navbar `}>
             <div className="nav">
                 <div className="div1">
-                    <h4 className="logo">Enigma</h4>
+                    <a className="logo" href="/">
+                        <img src="../images/enigma.png" alt="logo" />
+                        <p>Enigma</p>
+                    </a>
                 </div>
                 <div className="menu">
                     <ul className="tabs">
@@ -38,8 +40,9 @@ function Navbar() {
                 </div>
                 <div className={`hmenu ${open ? "text-gray-900" : "text-gray-100"}`}
                 onClick={()=>setOpen(!open)}>
-                    <FiMenu />
+                     {open ? <MdClose /> : <FiMenu />}
                 </div>
+                
                 <div className="M-menu" style={{right:open?"0":"-100%"}}>
                     <ul className="M-tabs">
                         {
